1dc18d3eea6164431eba193351968aeb8d0e8436
Erik Olofsson <erik@olofsson.info>
2019-10-13 18:04:57 +0200

    Use OpenSSL for MD5 by default

--- libarchive/archive_digest_private.h
+++ libarchive/archive_digest_private.h
@@ -174,7 +174,9 @@ typedef struct {
 #endif
 
 /* typedefs */
-#if defined(ARCHIVE_CRYPTO_MD5_LIBC)
+#if defined(ARCHIVE_CRYPTO_MD5_OPENSSL)
+typedef EVP_MD_CTX *archive_md5_ctx;
+#elif defined(ARCHIVE_CRYPTO_MD5_LIBC)
 typedef MD5_CTX archive_md5_ctx;
 #elif defined(ARCHIVE_CRYPTO_MD5_LIBMD)
 typedef MD5_CTX archive_md5_ctx;
@@ -184,8 +186,6 @@ typedef CC_MD5_CTX archive_md5_ctx;
 typedef mbedtls_md5_context archive_md5_ctx;
 #elif defined(ARCHIVE_CRYPTO_MD5_NETTLE)
 typedef struct md5_ctx archive_md5_ctx;
-#elif defined(ARCHIVE_CRYPTO_MD5_OPENSSL)
-typedef EVP_MD_CTX *archive_md5_ctx;
 #elif defined(ARCHIVE_CRYPTO_MD5_WIN)
 typedef Digest_CTX archive_md5_ctx;
 #else
