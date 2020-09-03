0455041d428dea6bf894cb25dc70e1000794567e
Kyle Evans <kevans@FreeBSD.org>
2020-09-03 14:46:01 -0500

    Retire use of O_EXEC for directories on FreeBSD
    As of FreeBSD 11.4 and FreeBSD 12.2, O_SEARCH is now provided. While it is
    defined as O_EXEC, it is better to retire the notion that O_EXEC will get
    the job done in case that changes in the future. This is also consistent
    with other platforms that provide O_PATH/O_SEARCH.

--- libarchive/archive_read_disk_posix.c
+++ libarchive/archive_read_disk_posix.c
@@ -2168,9 +2168,6 @@ tree_reopen(struct tree *t, const char *path, int restore_time)
 #elif defined(O_SEARCH)
 	/* SunOS */
 	const int o_flag = O_SEARCH;
-#elif defined(__FreeBSD__) && defined(O_EXEC)
-	/* FreeBSD */
-	const int o_flag = O_EXEC;
 #endif
 
 	t->flags = (restore_time != 0)?needsRestoreTimes:0;
@@ -2194,8 +2191,7 @@ tree_reopen(struct tree *t, const char *path, int restore_time)
 	t->stack->flags = needsFirstVisit;
 	t->maxOpenCount = t->openCount = 1;
 	t->initial_dir_fd = open(".", O_RDONLY | O_CLOEXEC);
-#if defined(O_PATH) || defined(O_SEARCH) || \
- (defined(__FreeBSD__) && defined(O_EXEC))
+#if defined(O_PATH) || defined(O_SEARCH)
 	/*
 	 * Most likely reason to fail opening "." is that it's not readable,
 	 * so try again for execute. The consequences of not opening this are
